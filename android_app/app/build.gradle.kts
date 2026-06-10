da13413760393718f30565d14bed68f6e8ec185d
DESKTOP-ULB7JN1\root <ifkiller@hotmail.com>
2026-06-10 20:35:30 +0900

    1.0.1(2) 버전처리

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.chingu.worklink"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.1.0"
         buildConfigField("String", "WEB_URL", "\"${webUrl}\"")
         buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"${googleWebClientId}\"")
     }
